5516c02eec3be1a8eba2e8026f174d47eb86004e
Nicole0101 <33416598+Nicole0101@users.noreply.github.com>
2026-05-05 03:41:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-REPORT_TYPE = "持股"
+REPORT_TYPE = "待留意買股"
 CSV_FILE = "stocks.csv"
-REPORT_TITLE = "(持股)技術分析報告"
-OUTPUT_FILE = "持股"
+REPORT_TITLE = "(待留意買股)技術分析報告"
+OUTPUT_FILE = "待留意買股"
 STATIC_OUTPUT_FILE = "AllStatic.csv"
